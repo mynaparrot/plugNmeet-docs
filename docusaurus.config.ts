e00815de02e1ba30a1d78a6695f8daccc9b6ea61
Jibon L. Costa <jiboncosta57@gmail.com>
2023-11-01 21:24:55 +0100

    fixed

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -38,7 +38,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/mynaparrot/plugNmeet-docs/edit/main/docs/',
+            'https://github.com/mynaparrot/plugNmeet-docs/edit/main/',
         },
         theme: {
           customCss: './src/css/custom.css',
